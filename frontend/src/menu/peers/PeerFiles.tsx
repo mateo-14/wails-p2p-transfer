cb178af4bb96602256e589090519b57e4aa4aed3
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2023-02-02 00:32:51 -0300

    Updaet PeerFiles

--- frontend/src/menu/peers/PeerFiles.tsx
+++ frontend/src/menu/peers/PeerFiles.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import { useState } from "react";
-import { DownloadFile } from "../../../wailsjs/go/main/App";
+// import { DownloadFile } from "../../../wailsjs/go/main/App";
 import { main } from "../../../wailsjs/go/models";
 
 type PeerFilesProps = {
@@ -12,7 +12,7 @@ export default function PeerFiles({ files, peerId }: PeerFilesProps) {
   const [selectedFile, setSelectedFile] = useState<string | null>(null);
 
   const handleClick = (file: main.PeerFile) => {
-    DownloadFile(peerId, file.path)
+    // DownloadFile(peerId, file.path)
   }
   return (
     <div className="h-full grid grid-cols-[1fr_300px]">
@@ -22,7 +22,7 @@ export default function PeerFiles({ files, peerId }: PeerFilesProps) {
         })}
       >
         {files.map(file => (
-          <button className="aspect-square rounded-md bg-zinc-900/50 flex items-end justify-center text-xs px-2 py-1 break-all text-center hover:bg-purple-600/30 select-none" onClick={() => handleClick(file)} key={file.path}>
+          <button className="aspect-square rounded-md bg-zinc-900/50 flex items-end justify-center text-xs px-2 py-1 break-all text-center hover:bg-purple-600/30 select-none" onClick={() => handleClick(file)} key={file.id}>
             {file.name}
           </button>
         ))}
